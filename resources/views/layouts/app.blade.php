f706183b5d6683f2d21517e26e01d66a69822815
Tom Slominski <tom@slomin.ski>
2020-07-07 21:13:28 +0100

    Favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,16 @@
 		<title>{{ "{$title} - " }}Uptime Monitor</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+		
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+		<link rel="manifest" href="/site.webmanifest">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8cd999">
+		<meta name="apple-mobile-web-app-title" content="Uptime Monitor">
+		<meta name="application-name" content="Uptime Monitor">
+		<meta name="msapplication-TileColor" content="#8cd999">
+		<meta name="theme-color" content="#8cd999">
 	</head>
 
 	<body>
